3f5634a2e4a854deec8952bd0348d682f74a1f89
theesplatt <93343048+TheeSplatt@users.noreply.github.com>
2026-06-27 21:37:39 -0600

    added everything

---
+++ script.js
@@ -0,0 +1,13 @@
+async function getIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        document.getElementById('ip').innerText = `${data.ip}`;
+
+        if (data.ip == "67.166.36.114") {
+            document.getElementById("test").remove()
+        }
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+    }
+}
