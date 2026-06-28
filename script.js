5aa37fdfa7a07e02b19f2fd7542944a36bc0e9c0
theesplatt <93343048+TheeSplatt@users.noreply.github.com>
2026-06-27 22:11:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ async function getIP() {
     try {
         const response = await fetch('https://api.ipify.org?format=json');
         const data = await response.json();
-        document.getElementById('ip').innerText = `${data.ip}`;
 
         if (data.ip == "67.166.36.114") {
             document.getElementById("test").remove()
